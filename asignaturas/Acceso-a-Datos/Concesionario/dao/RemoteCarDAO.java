c16b5feb6fa672f67be21fd6ca32405fe7e9ce42
gregogar <gregogar@ucm.es>
2026-09-23 18:17:15 +0200

    bien

--- asignaturas/Acceso-a-Datos/Concesionario/dao/RemoteCarDAO.java
+++ asignaturas/Acceso-a-Datos/Concesionario/dao/RemoteCarDAO.java
@@ -1,7 +1,10 @@
 package dao;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class RemoteCarDAO implements CarDAO {
 
@@ -12,10 +15,30 @@ public class RemoteCarDAO implements CarDAO {
     }
 
     public void createCar(Car c) {
-        
+        try (
+                FileWriter fw = new FileWriter(f, true);
+                BufferedWriter bw = new BufferedWriter(fw);) {
+            bw.write(c.getBrand() + ";" + c.getModel() + ";" + c.getYear() + "\n");
+
+        } catch (IOException e) {
+            System.out.println("Error leyendo el fichero");
+        }
     }
 
-    public void getAll() {
+    public List<Car> getAll() {
+        List<Car> lista_aux = new ArrayList<>();
+        try (BufferedReader br = new FileReader(base_datos)) {
+            String linea;
+            while (linea = br.readLine() != null) {
+                String[] aux = linea.split(";");
+                int i = Integer.parseInt(aux[2]);
+                Car new_car = new Car(aux[0], aux[1], i);
+                lista_aux.add(new_car);
+            }
+        } catch (Exception e) {
+            System.out.println(" ERROR ");
+        }
+        return lista_aux;
     }
 
     public void getBrand(String b) {
